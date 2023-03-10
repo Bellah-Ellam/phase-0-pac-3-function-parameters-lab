eb85b820c72ba570fbd7d8d7770302a3c384d8b6
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-10 09:08:21 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+// const language = "JavaScript"
+
+function introductionWithLanguage(name,language="JavaScript") {
+    
+
+    console.log("Hi, My name is ",name, " and I am learning to program in",language)
+}
+
+introductionWithLanguage('Bellah')
